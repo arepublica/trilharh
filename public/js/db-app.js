31d94f5c6ee44b3894e8e376d6569dfd83dfeabd
Paloma Barroso <palomapbarroso@gmail.com>
2019-07-01 20:39:24 -0300

    V1.9
    Alterações estilo post.

--- public/js/db-app.js
+++ public/js/db-app.js
@@ -1,11 +1,11 @@
-//var salvarLead = document.getElementById('salvarlead');
-var leadnome = document.getElementById('lead-nome');
-var leademail = document.getElementById('lead-email');
+//let salletLead = document.getElementById('salletlead');
+let leadnome = document.getElementById('lead-nome');
+let leademail = document.getElementById('lead-email');
 
 $(document).ready(function () {
     $.getJSON('http://ip-api.com/json?callback=?', function (data) {
         let leadIp = "";
-        var e = JSON.stringify(data.query, null, 2);
+        let e = JSON.stringify(data.query, null, 2);
         leadIp = e
        // alert(leadIp)
     });
@@ -13,49 +13,44 @@ $(document).ready(function () {
 
 
 function fSendLead() {
-    /* var ipCli=""
-     $.getJSON('http://ip-api.com/json?callback=?', function (data) {
-         var e = JSON.stringify(data.query, null, 2);
-         ipCli = e
-     });*/
-    var data = new Date();
-    // Guarda cada pedaço em uma variável
-    var dia = data.getDate();           // 1-31
-    var mes = data.getMonth();          // 0-11 (zero=janeiro)
-    var ano4 = data.getFullYear();       // 4 dígitos
-    var hora = data.getHours();          // 0-23
-    var min = data.getMinutes();        // 0-59
-    var seg = data.getSeconds();        // 0-59
+    let data = new Date();
+    // Guarda cada pedaço em uma letiável
+    let dia = data.getDate();           // 1-31
+    let mes = data.getMonth();          // 0-11 (zero=janeiro)
+    let ano4 = data.getFullYear();       // 4 dígitos
+    let hora = data.getHours();          // 0-23
+    let min = data.getMinutes();        // 0-59
+    let seg = data.getSeconds();        // 0-59
 
     if (mes <= 9) {
         mes = mes + 1
-        var m = '0' + mes
+        let m = '0' + mes
         mes = m
     };
 
     if (dia <= 9) {
-        var d = '0' + dia
+        let d = '0' + dia
         dia = d
     };
 
     if (hora <= 9) {
-        var h = '0' + hora
+        let h = '0' + hora
         hora = h
     };
     if (min <= 9) {
-        var mi = '0' + min
+        let mi = '0' + min
         min = mi
     };
 
     if (seg <= 9) {
-        var s = '0' + seg
+        let s = '0' + seg
         seg = s
     };
 
-    var str_hora = hora + ':' + min + ':' + seg;
-    var str_data = ano4 + '-' + (mes) + '-' + dia + ' ' + str_hora;
+    let str_hora = hora + ':' + min + ':' + seg;
+    let str_data = ano4 + '-' + (mes) + '-' + dia + ' ' + str_hora;
 
-    var lead = {
+    let lead = {
         nome: leadnome.value,
         email: leademail.value,
         dtInsc: str_data,
