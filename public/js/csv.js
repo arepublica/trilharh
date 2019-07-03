0cf9d60104f473c24464f7862707cb2add652ede
Paloma Barroso <palomapbarroso@gmail.com>
2019-07-03 12:09:50 -0300

    V3.7
    Adição de ícones, alteração do menu para ficar responsivo quando adicionar itens.

--- public/js/csv.js
+++
@@ -1,7 +0,0 @@
-
-firebase.database().ref('leads').on('value', function (snapshot) {
-    lista.innerHTML = '';
-    snapshot.forEach(function (item) {
-        lista.innerHTML += '<li>' + item.val().nome + ';' + item.val().email +  ';' +item.val().dtInsc +'</li>';
-    });
-});
